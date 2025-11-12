a369ca25ca13e53e47f13ae087076fab1841b8f5
alesjj10 <alesjukl@seznam.cz>
2025-11-12 08:26:24 +0000

    ukol 5

--- fifth.py
+++ fifth.py
@@ -6,49 +6,41 @@ gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
 png_header = b'\x89PNG\r\n\x1a\n'
 
-
 def read_header(file_name, header_length):
     """
-    Tato funkce načte binární soubor z cesty file_name,
-    z něj přečte prvních header_length bytů a ty vrátí pomocí return
+    Načte binární soubor a vrátí prvních x (header_length) bajtů.
     """
-    return b'xxx'
+    with open(file_name, 'rb') as soubor:
+        return soubor.read(header_length)
 
 
 def is_jpeg(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
-    tu srovná s definovanou hlavičkou v proměnné jpeg_header
+    Vrátí True, pokud je soubor typu JPEG.
     """
-    # načti hlavičku souboru
     header = read_header(file_name, len(jpeg_header))
-
-    # vyhodnoť zda je soubor jpeg
-
-    return False
+    return header == jpeg_header
 
 
 def is_gif(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
-    tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
+    Vrátí True, pokud je soubor typu GIF.
     """
-    # vyhodnoť zda je soubor gif
-    return False
+    header = read_header(file_name, len(gif_header1))
+    return header == gif_header1 or header == gif_header2
 
 
 def is_png(file_name):
     """
-    Funkce zkusí přečíst ze souboru hlavičku obrázku jpeg,
-    tu srovná s definovanou hlavičkou v proměnné png_header
+    Vrátí True, pokud je soubor typu PNG.
     """
-    # vyhodnoť zda je soubor png
-    return False
+    header = read_header(file_name, len(png_header))
+    return header == png_header
 
 
 def print_file_type(file_name):
     """
-    Funkce vypíše typ souboru - tuto funkci není třeba upravovat
+    Funkce vypíše typ souboru.
     """
     if is_jpeg(file_name):
         print(f'Soubor {file_name} je typu jpeg')
@@ -60,7 +52,15 @@ def print_file_type(file_name):
         print(f'Soubor {file_name} je neznámého typu')
 
 
-if __name__ == '__main__':
+
     # přidej try-except blok, odchyť obecnou vyjímku Exception a vypiš ji
-    file_name = sys.argv[1]
-    print_file_type(file_name)
+if __name__ == '__main__':
+    try:
+        file_name = sys.argv[1]
+        print_file_type(file_name)
+    except FileNotFoundError:
+        print('Chyba: Soubor nebyl nalezen.')
+    except IndexError:
+        print('Chyba: Zadejte cestu k souboru jako argument.')
+    except Exception as e:
+        print(f'Nastala neočekávaná chyba: {e}')
