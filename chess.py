19992e00b7c7dd2ec2b078cca9509749f1b69b05
alesjj10 <alesjukl@seznam.cz>
2025-12-03 15:51:25 +0000

    ukol7

---
+++ chess.py
@@ -0,0 +1,174 @@
+from abc import ABC, abstractmethod
+
+class Piece(ABC):
+    def __init__(self, color, position):
+        """
+        Inicializuje šachovou figurku.
+        
+        :param color: Barva figurky ('white' nebo 'black').
+        :param position: Aktuální pozice na šachovnici jako tuple (row, col).
+        """
+        self.__color = color
+        self.__position = position
+
+    @abstractmethod
+    def possible_moves(self):
+        """
+        Vrací všechny možné pohyby figurky.
+        Musí být implementováno v podtřídách.
+        
+        :return: Seznam možných pozic [(row, col), ...].
+        """
+        pass
+
+    @staticmethod
+    def is_position_on_board(position):
+        return 1 <= position[0] <= 8 and 1 <= position[1] <= 8
+
+    @property
+    def color(self):
+        return self.__color
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, new_postion):
+        self.__position = new_postion
+
+    def __str__(self):
+        return f'Piece({self.color}) at position {self.position}'
+
+
+class Pawn(Piece):
+    def possible_moves(self):
+        row, col = self.position
+
+        if self.color == "white":
+            moves = [(row + 1, col)]
+        else:
+            moves = [(row - 1, col)]
+
+        # Filtruje tahy, které jsou mimo šachovnici    
+        final_moves = []
+        for move in moves:
+            if self.is_position_on_board(move):
+                final_moves.append(move)
+        return final_moves
+
+
+    def __str__(self):
+        return f'Pawn({self.color}) at position {self.position}'
+
+
+class Knight(Piece):
+    def possible_moves(self):
+        """
+        Vrací všechny možné tahy jezdce.
+        
+        :return: Seznam možných pozic [(row, col), ...].
+        """
+        row, col = self.position
+        moves = [
+            (row + 2, col + 1), (row + 2, col - 1),
+            (row - 2, col + 1), (row - 2, col - 1),
+            (row + 1, col + 2), (row + 1, col - 2),
+            (row - 1, col + 2), (row - 1, col - 2)
+        ]
+        # Filtruje tahy, které jsou mimo šachovnici
+        final_moves = []
+        for move in moves:
+            if self.is_position_on_board(move):
+                final_moves.append(move)
+        return final_moves
+
+    def __str__(self):
+        return f'Knight({self.color}) at position {self.position}'
+
+
+class Bishop(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = []
+
+        for i in range(1, 8):
+            moves.append((row + i, col + i))
+            moves.append((row - i, col - i))
+            moves.append((row + i, col - i))
+            moves.append((row - i, col + i))
+
+        final_moves = []
+        for move in moves:
+            if self.is_position_on_board(move):
+                final_moves.append(move)
+        return final_moves
+
+    def __str__(self):
+        return f'Bishop({self.color}) at position {self.position}'
+
+class Rook(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = []
+
+        for r in range(1, 9):
+            if r != row:
+                moves.append((r, col))
+
+        for c in range(1,9):
+            if c != col:
+                moves.append((row, c))
+
+        final_moves = []
+        for move in moves:
+            if self.is_position_on_board(move):
+                final_moves.append(move)
+        return final_moves
+
+    def __str__(self):
+        return f'Rook({self.color}) at position {self.position}'
+
+
+class Queen(Piece):
+    def possible_moves(self):
+        rook_moves = Rook(self.color, self.move).possible_moves()
+        bishop_moves = Bishop(self.color, self.move).possible_moves()
+
+        moves = rook_moves + bishop_moves
+
+        final_moves = []
+        for move in moves:
+            if self.is_position_on_board(move):
+                final_moves.append(move)
+        return final_moves
+
+    def __str__(self):
+        return f'Queen({self.color}) at position {self.position}'
+
+
+
+class King(Piece):
+    def possible_moves(self):
+        row, col = self.position
+        moves = [
+            (row + 1, col), (row - 1, col),
+            (row, col + 1), (row, col - 1),
+            (row + 1, col + 1), (row - 1, col - 1),
+            (row + 1, col - 1), (row - 1, col +1)
+        ]
+
+        final_moves = []
+        for move in moves:
+            if self.is_position_on_board(move):
+                final_moves.append(move)
+        return final_moves
+
+    def __str__(self):
+        return f'King({self.color}) at position {self.position}'
+
+
+if __name__ == "__main__":
+    piece = Knight("black", (1, 2))
+    print(piece)
+    print(piece.possible_moves())
